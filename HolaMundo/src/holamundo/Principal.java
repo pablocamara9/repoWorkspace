8a23a61cc8a420b8e2d7811cfcca96af65c6d440
Pablo Cámara García <camara.gapab23@triana.salesianos.edu>
2023-11-20 13:03:54 +0100

    Creado el proyecto HolaMundo

---
+++ HolaMundo/src/holamundo/Principal.java
@@ -0,0 +1,12 @@
+package holamundo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo");
+
+	}
+
+}
