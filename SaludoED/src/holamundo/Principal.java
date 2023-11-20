1701d3de87b57fa31dc51a41e1e5f8763ac1d3c5
Pablo Cámara García <camara.gapab23@triana.salesianos.edu>
2023-11-20 13:27:06 +0100

    Saludo a los alumnos de ED

---
+++ SaludoED/src/holamundo/Principal.java
@@ -0,0 +1,11 @@
+package holamundo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola a todos los alumnos de Entornos de Desarrollo, menos a algunos :)");
+
+	}
+
+}
