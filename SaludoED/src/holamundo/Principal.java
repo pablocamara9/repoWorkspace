00b1635b844a442927153597b633bab0538a2779
Pablo Cámara García <camara.gapab23@triana.salesianos.edu>
2023-11-21 14:05:19 +0100

    Se mejora el saludo a los alumnos

--- SaludoED/src/holamundo/Principal.java
+++ SaludoED/src/holamundo/Principal.java
@@ -4,8 +4,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Hola a todos los alumnos de Entornos de Desarrollo, menos a algunos :)");
+		System.out.println("-".repeat(50));
+		System.out.println("Hola a todos los alumnos de Entornos de Desarrollo");
+		System.out.println("-".repeat(50));
 
 	}
 
